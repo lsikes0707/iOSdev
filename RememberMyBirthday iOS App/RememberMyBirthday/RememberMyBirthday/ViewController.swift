4ec911e36b7d05bf093f26d8b8e8d8899cf38692
Lacey Gruwell (Sikes) <lacey_sikes@my.cuesta.edu>
2024-08-27 09:54:51 -0700

    Adding in RememberMyBirthday app, init

---
+++ RememberMyBirthday iOS App/RememberMyBirthday/RememberMyBirthday/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RememberMyBirthday
+//
+//  Created by Lacey Gruwell on 8/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
